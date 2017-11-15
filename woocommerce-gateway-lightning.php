2a87a340f3ba5b41e52c93242ecbc5b90f28e11c
Nadav Ivgi <nadav@shesek.info>
2017-11-15 23:33:03 +0200

    move gateway registration inside the constructor

--- woocommerce-gateway-lightning.php
+++ woocommerce-gateway-lightning.php
@@ -67,6 +67,7 @@ if (!function_exists('init_wc_lightning')) {
         // Lightning Strike REST client
         $this->strike = new LightningStrikeClient($this->get_option('server_url', 'http://localhost:8009'));
 
+        add_action('woocommerce_payment_gateways', array($this, 'register_gateway'));
         add_action('woocommerce_api_wc_gateway_lightning', array($this, 'webhook_callback'));
         add_action('woocommerce_receipt_lightning', array($this, 'show_payment_info'));
         add_action('woocommerce_thankyou_lightning', array($this, 'show_payment_info'));
@@ -194,8 +195,7 @@ if (!function_exists('init_wc_lightning')) {
       }
     }
 
-    $gateway = new WC_Gateway_Lightning();
-    add_action('woocommerce_payment_gateways', array($gateway, 'register_gateway'));
+    new WC_Gateway_Lightning();
   }
 
   add_action('plugins_loaded', 'init_wc_lightning');

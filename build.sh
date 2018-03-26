fbcd540753a6cdfce5f24fd6b88bf89dbc6cb9eb
Nadav Ivgi <nadav@shesek.info>
2018-03-26 10:52:33 +0300

    Add README.md to distribution zip

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 composer install --no-dev
-zip -r woocommerce-gateway-lightning.zip composer.json woocommerce-gateway-lightning.php vendor templates css img --exclude='vendor/bacon/bacon-qr-code/tests/*'
+zip -r woocommerce-gateway-lightning.zip composer.json woocommerce-gateway-lightning.php vendor templates css img README.md --exclude='vendor/bacon/bacon-qr-code/tests/*'
 

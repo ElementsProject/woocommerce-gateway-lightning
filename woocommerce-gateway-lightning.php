8d2ba523cc92861c19268308785c03301b1a89c3
Nadav Ivgi <nadav@shesek.info>
2018-08-05 04:42:26 +0300

    v0.2.6

--- woocommerce-gateway-lightning.php
+++ woocommerce-gateway-lightning.php
@@ -6,7 +6,7 @@
     Author:      Blockstream
     Author URI:  https://blockstream.com
 
-    Version:           0.2.5
+    Version:           0.2.6
     GitHub Plugin URI: https://github.com/ElementsProject/woocommerce-gateway-lightning
 */
 
